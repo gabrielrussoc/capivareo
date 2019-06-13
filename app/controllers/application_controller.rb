6cd1408077aa25a7a023c73d819071f987a6babd
Victor Sprengel <victor.sprengel@gmail.com>
2019-06-13 15:20:38 -0300

    delay articial em dev para simular o comportamento dos spinners

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,14 @@ class ApplicationController < ActionController::Base
     # https://github.com/gabrielrussoc/capivareo/issues/1
     skip_before_action :verify_authenticity_token
     respond_to :json
+  
+    before_action :dev_delay
+
+    private
+    # adds 1s delay only if in development env
+    def dev_delay 
+        if Rails.env.development?
+            sleep 1
+        end
+    end
 end
