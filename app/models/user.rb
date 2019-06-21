6cb0b1eef18523db0c6381f1775e1cc30d6d2b39
Gabriel Russo <gabrielrcarmo@gmail.com>
2019-06-21 11:47:46 -0300

    melhor query para notas

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :disciplinas
+  has_many :notas
   validates :nusp, presence: true
   validates :nusp, format: { with: /\A\d*\z/, message: "only allows numbers" }
   validates_uniqueness_of :nusp
