d03c16d80c2e9ed148ff911f54096f4bd897e038
Gabriel Russo <gabrielrcarmo@gmail.com>
2019-06-21 14:34:22 -0300

    validacoes nos modelos

--- app/models/atividade.rb
+++ app/models/atividade.rb
@@ -1,4 +1,5 @@
 class Atividade < ApplicationRecord
   belongs_to :disciplina
   has_many :notas
+  validates :nome, presence: true
 end
--- app/models/disciplina.rb
+++ app/models/disciplina.rb
@@ -2,4 +2,15 @@ class Disciplina < ApplicationRecord
     belongs_to :user
     has_and_belongs_to_many :users
     has_many :atividades
+    
+    validates :cod, presence: true
+    validates :nome, presence: true
+    validates :semestre, presence: true
+    validate :valid_semestre 
+
+    def valid_semestre
+        if semestre.day != 1 || semestre.month != 1 || semestre.month != 7
+            errors.add(:semestre, "must be jan 1st or jul 1st")
+        end
+    end
 end
