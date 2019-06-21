4f7ae649944ff48a9d324b0d19085ada5a8ccc10
Victor Sprengel <victor.sprengel@gmail.com>
2019-06-21 20:39:18 -0300

    nota modelo teste

--- app/models/nota.rb
+++ app/models/nota.rb
@@ -2,5 +2,5 @@ class Nota < ApplicationRecord
   belongs_to :atividade
   belongs_to :user
 
-  validates :nota, :inclusion => 0..100
+  validates :nota, :inclusion => 0..100, numericality: { only_integer: true }
 end
