43b01f6c87a62f2106d5bbc2c513f85cbf9dd4f5
Gabriel Russo <gabrielrcarmo@gmail.com>
2019-06-23 23:57:35 -0300

    filtrando arquivos que no simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,8 @@
 require 'simplecov'
-SimpleCov.start 'rails'
+SimpleCov.start 'rails' do
+  add_filter '/app/channels/'
+  add_filter '/app/jobs/application_job.rb'
+end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
